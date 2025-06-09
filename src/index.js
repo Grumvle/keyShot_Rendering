c3d67d2ae8458fbe9389c8e026598d5daebcc0ef
Grumble <hujik1234@gmail.com>
2025-06-09 23:02:25 +0900

    feat: 외부접속 가능하도록 수정

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`✅ KeyShot Render Server 실행 중: http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`✅ KeyShot Render Server 실행 중`);
 });
